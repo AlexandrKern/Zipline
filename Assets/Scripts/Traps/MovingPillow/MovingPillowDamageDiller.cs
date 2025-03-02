97fd6c28b7f01af08e8002bc6ebe74b5392f74a1
AlexandrKern <137929102+AlexandrKern@users.noreply.github.com>
2025-03-02 19:51:57 +0300

    add traps

---
+++ Assets/Scripts/Traps/MovingPillow/MovingPillowDamageDiller.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MovingPillowDamageDiller : MonoBehaviour
+{
+    private PathToEndPoint _pathToEndPoint;
+
+    public virtual void Start()
+    {
+        _pathToEndPoint = PathToEndPoint.Instance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Cat cat = collision.GetComponent<Cat>();
+        if (cat != null)
+        {
+            _pathToEndPoint.StopCatOnPath(cat);
+        }
+        else
+        {
+            Debug.Log("Это явно не кот");
+        }
+
+    }
+}
